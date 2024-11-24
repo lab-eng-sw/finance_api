57f0ab7b07a800e0f8a2d1c6acc71c552e0dd9e8
Marcelo Almeida <marcelo.aallmeida@gmail.com>
2024-11-23 22:54:10 -0300

    feature: enabled cors

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors()
 }
 bootstrap();
