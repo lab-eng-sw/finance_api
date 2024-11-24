e28dd7487d0ef0f8191dcb8829e57e6bd85886cd
Marcelo Almeida <marcelo.aallmeida@gmail.com>
2024-11-23 23:12:36 -0300

    feature: allowed all headers

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type',
+    allowedHeaders: '*',
   });
 }
 bootstrap();
